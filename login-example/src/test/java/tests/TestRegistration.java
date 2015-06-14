8c142e9355bad2a78cde3f617000e4a01914ab0c
itaiag <itai.agmon@gmail.com>
2015-06-14 23:25:12 +0300

    Fix

--- login-example/src/test/java/tests/TestRegistration.java
+++ login-example/src/test/java/tests/TestRegistration.java
@@ -31,8 +31,8 @@ public class TestRegistration extends InitializeDriverTestCase {
 		loginPage.typeToPasswordTb(PASSWORD);
 		HomePage homePage = loginPage.clickOnLoginBtnAndGoToHomePage();
 
-		homePage.clickOnLogoutBtnAndGoToHomePage();
-
+		homePage.clickOnLogoutBtnAndGoToLoginPage();
+		
 	}
 
 	@Test
