dc2666d5e3ecacc267bc654b9b485d1b4abdcf13
itaiag <itai.agmon@gmail.com>
2015-06-18 13:28:10 +0300

    Removed warnings

--- login-example/src/test/java/systemobjects/RegisterPage.java
+++ login-example/src/test/java/systemobjects/RegisterPage.java
@@ -2,9 +2,6 @@ package systemobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegisterPage extends AbstractPage {
 
